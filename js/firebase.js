ef8d54a93037d7d4b40239f471383f25cc5e6873
DalubwikaanNEU <ycherochristo.amancio@neu.edu.ph>
2026-07-23 02:22:35 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,50 +1,57 @@
-// Firebase App
+// FIREBASE CONFIG
+
 import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
 
-// Firebase Firestore
 import { getFirestore } 
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/11.0.2/firebase-storage.js";
 
 
-// Your Firebase Configuration
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyBzo7AfReNUHrXf0FEqfAW5h0oQ-fN1ij8",
+apiKey: "AIzaSyBzo7AfReNUHrXf0FEqfAW5h0oQ-fN1ij8",
 
-  authDomain: "dalubwikaan-system.firebaseapp.com",
+authDomain:"dalubwikaan-system.firebaseapp.com",
 
-  projectId: "dalubwikaan-system",
+projectId:"dalubwikaan-system",
 
-  storageBucket: "dalubwikaan-system.firebasestorage.app",
+storageBucket:"dalubwikaan-system.firebasestorage.app",
 
-  messagingSenderId: "74868534824",
+messagingSenderId:"74868534824",
 
-  appId: "1:74868534824:web:06b15dacd299d69e754f1d"
+appId:"1:74868534824:web:06b15dacd299d69e754f1d"
 
 };
 
 
 
 
-// Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
-
-
-
-// Initialize Firestore Database
-
-const db = getFirestore(app);
+const app =
+initializeApp(firebaseConfig);
 
 
 
+const db =
+getFirestore(app);
 
-// Export Database
 
-export { db };
+
+const storage =
+getStorage(app);
+
+
+
+
+export {
+
+db,
+storage
+
+};
