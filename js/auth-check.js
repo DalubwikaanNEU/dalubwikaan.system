e6538dedc4bad72c9d165ec116a6f633ea2a21c1
DalubwikaanNEU <ycherochristo.amancio@neu.edu.ph>
2026-07-23 02:36:42 +0800

    Create auth-check.js

---
+++ js/auth-check.js
@@ -0,0 +1,31 @@
+import {
+
+getAuth,
+onAuthStateChanged
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+
+
+const auth = getAuth();
+
+
+
+onAuthStateChanged(auth,(user)=>{
+
+
+if(!user){
+
+
+window.location.href="login.html";
+
+
+}
+
+
+
+});
